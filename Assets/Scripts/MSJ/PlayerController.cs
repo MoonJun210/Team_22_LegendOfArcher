db34a331c0f737c21c27990fd98bf6af9bf1bdd0
MoonJun210 <moonjun210@naver.com>
2025-05-08 19:39:07 +0900

    플레이어 움직임

--- Assets/Scripts/MSJ/PlayerController.cs
+++ Assets/Scripts/MSJ/PlayerController.cs
@@ -1,8 +1,147 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.InputSystem;
 
 public class PlayerController : BaseController
 {
-    
+    private new Camera camera;
+
+    [SerializeField] private Transform weaponPivot;
+
+    private Vector2 knockback = Vector2.zero;
+    private float knockbackDuration = 0.0f;
+
+    protected PlayerAnimationHandler animationHandler;
+
+    protected PlayerStatHandler statHandler;
+
+    //[SerializeField] public WeaponHandler WeaponPrefab;
+    //protected WeaponHandler weaponHandler;
+
+    protected bool isAttacking;
+    private float timeSinceLastAttack = float.MaxValue;
+
+    protected override void Awake()
+    {
+        base.Awake();
+        statHandler = GetComponent<PlayerStatHandler>();
+        animationHandler = GetComponent <PlayerAnimationHandler >();
+        camera = Camera.main;
+    }
+
+    protected override void FixedUpdate()
+    {
+        base .FixedUpdate();
+        if (knockbackDuration > 0.0f)
+        {
+            knockbackDuration -= Time.fixedDeltaTime;
+        }
+    }
+
+
+    protected override void Movment(Vector2 direction)
+    {
+        direction = direction * statHandler.Speed; // 속도 조절은 여기서
+        if (knockbackDuration > 0.0f)
+        {
+            direction *= 0.2f;
+            direction += knockback;
+        }
+        _rigidbody.velocity = direction;
+    }
+
+    protected override void Rotate(Vector2 direction)
+    {
+        base.Rotate(direction);
+
+        float rotZ = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        bool isLeft = Mathf.Abs(rotZ) > 90f;
+        //if (weaponPivot != null)
+        //{
+        //    weaponPivot.rotation = Quaternion.Euler(0, 0, rotZ);
+        //}
+        //weaponHandler?.Rotate(isLeft);
+    }
+    public void ApplyKnockback(Transform other, float power, float duration)
+    {
+        knockbackDuration = duration;
+        knockback = -(other.position - transform.position).normalized * power;
+    }
+
+    private void HandleAttackDelay()
+    {
+        //if (weaponHandler == null)
+        //    return;
+
+        //if (timeSinceLastAttack <= weaponHandler.Delay)
+        //{
+        //    timeSinceLastAttack += Time.deltaTime;
+        //}
+
+        //if (isAttacking && timeSinceLastAttack > weaponHandler.Delay)
+        //{
+        //    timeSinceLastAttack = 0;
+        //    Attack();
+        //}
+    }
+
+    protected void Attack()
+    {
+        //if (lookDirection != Vector2.zero)
+        //    weaponHandler?.Attack();
+    }
+
+    public void Death()
+    {
+        _rigidbody.velocity = Vector3.zero;
+
+        foreach (SpriteRenderer renderer in transform.GetComponentsInChildren<SpriteRenderer>())
+        {
+            Color color = renderer.color;
+            color.a = 0.3f;
+            renderer.color = color;
+        }
+
+        foreach (Behaviour component in transform.GetComponentsInChildren<Behaviour>())
+        {
+            component.enabled = false;
+        }
+
+        Destroy(gameObject, 2f);
+    }
+
+
+    void OnMove(InputValue inputValue)
+    {
+        movementDirection = inputValue.Get<Vector2>();
+        movementDirection = movementDirection.normalized;
+    }
+
+    void OnLook(InputValue inputValue)
+    {
+
+        Vector2 mousePosition = inputValue.Get<Vector2>();
+        Vector2 worldPos = camera.ScreenToWorldPoint(mousePosition);
+        lookDirection = (worldPos - (Vector2)transform.position);
+
+        if (lookDirection.magnitude < .9f)
+        {
+            lookDirection = Vector2.zero;
+        }
+        else
+        {
+            lookDirection = lookDirection.normalized;
+        }
+    }
+
+    void OnFire(InputValue inputValue)
+    {
+        if (EventSystem.current.IsPointerOverGameObject())
+        {
+            return;
+        }
+        isAttacking = inputValue.isPressed;
+    }
 }
